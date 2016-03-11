12755445434db3800906334ddd4d25eb0bedde48
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-03-11 18:46:03 +0530

    Final demo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DrawPad
 A digital chalkboard
+
+
+mouse.php - 
+client_new.php - 
+
+#### Note:
+1. Do not change the name of main directory. It should be 'DrawPad'.
+2. If images do not save then check the permission of the 'mouse.php' and 'client_new.php'
+3. All files are organised in specific categories, css, js, png's and data files.
---
+++ svg_to_png.php
@@ -0,0 +1,15 @@
+<?php
+	$file="temp_svg.svg";
+	$o = $_POST["svg"];
+	$old = file_get_contents($file);
+	
+	$length = 7;
+	$randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+	//echo $randomString;
+
+	file_put_contents($file,$o);
+	system('rsvg-convert temp_svg.svg > export_png/'.$randomString.'.png');
+	echo 'http://'.$_SERVER['HTTP_HOST'].'/DrawPad/export_png/'.$randomString.'.png';
+	//echo file_put_contents( "data.json", json_encode($o) );
+?>
+
